6dad1fa695b6e07aa5122c3c71ab502d4c5992b2
Reshan Gayantha <92083897+reshangayantha9@users.noreply.github.com>
2026-03-04 15:55:33 +0530

    Add hfToken to environment settings
    Added hfToken to environment configuration.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'https://reshangayantha-mg-avf.hf.space'
+  baseUrl: 'https://reshangayantha-mg-avf.hf.space',
+  hfToken:'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'
 };
 
 /*
